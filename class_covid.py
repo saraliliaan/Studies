6d29aff1caa92e55fb563847b32fad7ed331bf8b
Sara Aguiar <110203845+saraliliaan@users.noreply.github.com>
2022-08-31 19:32:38 -0300

    Add files via upload

---
+++ class_covid.py
@@ -0,0 +1,29 @@
+class Covid():
+    def posicao(self):
+        print ('-------')
+    def nome(self):
+        print ('-------')
+    def casos(self):
+        print ('-------')
+    def mortes(self):
+        print ('-------')
+
+class Brasil(Covid):
+    def posicao(self):
+        print('3º lugar')
+    def nome(self):
+        print('-> Brasil - América do Sul')
+    def casos(self):
+        print('29.000.000 casos por covid-19')
+    def mortes(self):
+        print('658.000 mortes confirmadas por Covid-19')
+
+class main():
+    bra = Brasil()
+    print('Brasil: ')
+    print (bra.posicao)
+    bra.nome
+    bra.casos
+    bra.mortes
+
+if __name__ == "__main__": main()
