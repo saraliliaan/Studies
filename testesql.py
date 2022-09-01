635bc77915f23fe0bf5bb1df95690fc51c5b980d
Sara Aguiar <110203845+saraliliaan@users.noreply.github.com>
2022-09-01 14:16:50 -0300

    Update testesql.py

--- testesql.py
+++ testesql.py
@@ -1,7 +1,7 @@
 import sqlite3
 #importar banco de dados
 conn = sqlite3.connect('aulaBD.db')
-#adicionar valores no BD
+#insert - adicionar valores no BD
 cursor = conn.cursor()
 cursor.execute("""INSERT INTO fornecedor (nome.fornecedor, cnpj, cidade, estado, cep, data_cadastro)
 VALUES ('Empresa A', '11.111.111/1111-11, 'São Paulo', 'SP', '11111-111', '2020-01-01')
@@ -9,8 +9,16 @@ VALUES ('Empresa A', '11.111.111/1111-11, 'São Paulo', 'SP', '11111-111', '2020
 
 conn.commit()
 
-#ler valores no BD
+#read - ler valor no BD
 cursor.execute("SELECT * FROM fornecedor")
 resultado = cursor.fetchall()
 for linha in resultado:
     print(linha)
+
+#update - inserir um registro no BD
+cursor.execute("UPDATE fornecedor SET cidade = 'Campinas' WHERE id_fornecedor = 5")
+conn.commit()
+
+#delete - deletar um registro do BD
+cursor.execute("DELETE FROM fornecedor WHERE id_fornecedor = 2")
+conn.commit()
